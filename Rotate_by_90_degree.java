a4d3aecda57a6cb23452e0709b7acafe55a7309e
AmritJoshi <79530842+AmritJoshi@users.noreply.github.com>
2021-07-03 17:15:22 +0545

    Create Rotate_by_90_degree.java

---
+++ Rotate_by_90_degree.java
@@ -0,0 +1,53 @@
+/*
+Given a square matrix[][] of size N x N. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space.
+
+Example 1:
+
+Input:
+N = 3
+matrix[][] = [[1 2 3],
+              [4 5 6],
+              [7 8 9]]
+Output:
+3 6 9 
+2 5 8 
+1 4 7
+
+Your Task:
+You only need to implement the given function rotate(). Do not read input, instead use the arguments given in the function. 
+
+Expected Time Complexity: O(N*N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 50
+1 <= matrix[][] <= 100
+*/
+
+class GFG
+{
+    static void rotate(int matrix[][]) 
+    {
+        // Code Here
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        
+        for(int i=0;i<m/2;i++){
+            for(int j=0;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[m-i-1][j];
+                matrix[m-i-1][j]=temp;
+                
+            }
+        }
+    
+        
+    }
+}
