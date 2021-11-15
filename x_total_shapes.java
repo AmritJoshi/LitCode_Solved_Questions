3b29ef607ccd0c476fb0dc3236e42af23a312cf6
AmritJoshi <79530842+AmritJoshi@users.noreply.github.com>
2021-11-15 11:32:23 +0545

    Create x_total_shapes.java

---
+++ x_total_shapes.java
@@ -0,0 +1,109 @@
+/*Given  a grid of n*m consisting of O's and X's. The task is to find the number of 'X' total shapes.
+Note: 'X' shape consists of one or more adjacent X's (diagonals not included).
+ 
+
+Example 1:
+
+Input: grid = {{X,O,X},{O,X,O},{X,X,X}}
+Output: 3
+Explanation: 
+The grid is-
+X O X
+O X O
+X X X
+So, X with same colour are adjacent to each 
+other vertically for horizontally (diagonals 
+not included). So, there are 3 different groups 
+in the given grid.
+
+Example 2:
+
+Input: grid = {{X,X},{X,X}}
+Output: 1
+Expanation: 
+The grid is- 
+X X
+X X
+So, X with same colour are adjacent to each
+other vertically for horizontally (diagonals
+not included). So, there is only 1 group
+in the given grid.
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function xShape() which takes grid as input parameter and returns the count of total X shapes.
+ 
+
+Expected Time Compelxity: O(n*m)
+Expected Space Compelxity: O(n*m)
+ 
+
+Constraints:
+1 ≤ n, m ≤ 100
+*/
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            char[][] grid = new char[n][m];
+            for(int i = 0; i < n; i++){
+                String S = br.readLine().trim();
+                for(int j = 0; j < m; j++){
+                    grid[i][j] = S.charAt(j);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.xShape(grid);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the number of 'X' total shapes.
+    public int xShape(char[][] grid)
+    {
+        // code here
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='X'){
+                    count++;
+                    dfs(grid,i,j,m,n);
+                }
+            }
+        }
+        return count;
+    }
+    
+    
+    void dfs(char[][] grid, int i,int j,int m,int n){
+        
+        if(i>=n || i<0 || j>=m || j<0 || grid[i][j]!='X')
+            return;
+
+            grid[i][j]='O';
+        
+            dfs(grid,i+1,j,m,n);
+            dfs(grid,i,j+1,m,n);
+            dfs(grid,i-1,j,m,n);
+            dfs(grid,i,j-1,m,n);
+        
+    }
+}
